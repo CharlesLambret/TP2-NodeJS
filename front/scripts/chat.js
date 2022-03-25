3e69d00e00c4052df535124b42a5dba2c0f7a895
Charles Lambret <charleslambretpro@gmail.com>
2022-03-25 14:44:03 +0100

    ajout de la sécurité du formulaire v1

--- front/scripts/chat.js
+++ front/scripts/chat.js
@@ -15,6 +15,14 @@ ws.onopen = function open() {
 }
 ws.onmessage = handleWSMessage
 
+/* Securisation du chat en coupant les valeurs trop hautes avec un message d'erreur*/
+if ("pseudo".length > 25 || null) {
+  "pseudo".slice(0, 25);
+  alert("Tapez un pseudo valable (entre 1 et 25 caractères) pour avoir une réponse.")}
+if ("body".length > 240 || null) {
+  "pseudo".slice(0, 240);
+  alert("Veuillez rentrer un message compris entre 0 et 240 caractères.")}
+
 export function initChat() {
   /** @type {HTMLFormElement | null} */
   const messageForm = document.querySelector('#new-message')
@@ -22,7 +30,7 @@ export function initChat() {
 
   messageForm.addEventListener('submit', (event) => {
     event.preventDefault()
-
+  
     const pseudo = messageForm.pseudo.value
     const body = messageForm.body.value
 
