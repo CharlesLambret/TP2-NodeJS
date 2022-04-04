50eb2997848981562302c25500272330ce63c165
Bryangv03 <79462119+Bryangv03@users.noreply.github.com>
2022-04-04 11:07:25 +0200

    ca marche !!!!!!!!!!!!!!!!!!!!!!!!

--- front/scripts/chat.js
+++ front/scripts/chat.js
@@ -44,3 +44,25 @@ export function initChat() {
 
   })
 }
+
+/** Petit bouton Darkmode */
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
+
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
