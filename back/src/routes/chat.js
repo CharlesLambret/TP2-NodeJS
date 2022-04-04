9bb827bb7692ccdf9349855f80854e47bc05727a
Charles Lambret <79523370+Elektazze@users.noreply.github.com>
2022-04-04 09:26:55 +0200

    Rajout des fonctionnalités en travaux
    Rajout des fonctionnalités en travaux

--- back/src/routes/chat.js
+++ back/src/routes/chat.js
@@ -1,7 +1,6 @@
-import { randomUUID } from 'crypto';
-import { format, formatDistance, formatRelative, subDays } from 'date-fns';
+import { randomUUID } from 'crypto'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-/*const messageForm = document.querySelector('#new-message')*/
 
 /**
  * @typedef {Object} Message
@@ -11,32 +10,68 @@ import { format, formatDistance, formatRelative, subDays } from 'date-fns';
  */
 
 /** @type { Message[] } */
-
 const messages = []
+const date = new Date()
+//tableau pour stocker les messages envoyés
+//tableau =[]
+formatDistanceToNow(date, [options])
 
-/*let testdate = firstdate.getTime()
-let testdate1 = firstdate.getDate()
+JSON.stringify(date)
 
-console.log(firstdate.toString());
-console.log(firstdate.getTimezoneOffset());
-console.log(testdate1)
-*/
+/*Sécurisation du chat avec une limite de caractères*/
+const pseudo = document.getElementById("pseudo");
+const body = document.getElementById("body");
+
+/*Sécurisation du chat avec coupe des entrées si elles sont trop longues */
+const errormessages = []
+  if (pseudo.value > 25 || pseudo.value === null) {
+    pseudo.slice(0, 25);
+    errormessages.push("Tapez un pseudo valable (entre 1 et 25 caractères).")
+  }
+  if (body.value > 240 || body.value === null) {
+    body.slice(0, 240);
+    errormessages.push("Veuillez rentrer un message compris entre 0 et 240 caractères.")
+  }
+
+/*Tableau pour contenir les messages envoyés*/
+const savedmessages =[];
+
+/*Si le message est contenu dans le tableau il est affiché à droite sinon à gauche*/
+  savedmessages.includes(messages) ? messages.style.displayright : messages.style.displayleft;
+
+
+const deletelastmessage = savedmessages.pop();
 
 /**
  * @param {string} pseudo
  * @param {string} body
  */
 function handleNewMessage(pseudo, body) {
-  const now = format(new Date(), "'Sent at 'hh:mm:ss ")
   const message = {
     id: randomUUID(),
-    pseudo: pseudo,
-    body: body,
-    firstdate: now.toString(),
+    pseudo,
+    date,
+    body,
   }
   messages.push(message)
+/*Envoie les messages dans le tableau*/
+savemessages.append(message);
   return message
 }
+
+// matrice regroupant les utilisateurs que l'on nomme identification
+/**identifiant = [] //id de l'utilisateur
+mdp = [] //mdp de l'utilisateur
+
+identification = [identifiant][mdp]
+// pour pouvoir ajouter un nouvel utilisateur
+identication.append(id, mdp)
+// exemple de format pour la matrice
+identification = [[charlotte, zbuovbruo], [marc, jbsfveiu], [julie, zvobu]]
+
+// si l'utilisateur n'est pas nouveau il suffit de vérif s'il fait parti de la matrice et si oui autoriser la connection
+
+
 
 /**
  * @type { import('fastify').FastifyPluginCallback }
@@ -62,18 +97,3 @@ export async function chatRoutes(app) {
     })
   })
 }
-// secu chat inch'allah
-  
-/*const pseudo = messageForm.pseudo.value
-const body = messageForm.body.value
-let secupseudo = pseudo.slice(0, 25);
-let secubody = body.slice(0, 240);
-let errormessages = []
-if (pseudo > 25 || pseudo === null) {
-      secupseudo;
-      errormessages.push("Tapez un pseudo valable (entre 1 et 25 caractères).")
-    }
-if (body > 240 || body === null) {
-      secubody;
-      errormessages.push("Veuillez rentrer un message compris entre 0 et 240 caractères.")
-    }*/
